34dd48dae7f1e0c41dd5aef0ec3d363222436d9e
Sasi Datta <sasi.bitra@mitel.com>
2024-04-25 20:03:44 +0530

    ec2 ami image

--- ec2.tf
+++ ec2.tf
@@ -1,21 +1,6 @@
-# Find the latest Ubuntu 22.02 AMI
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 # Create the EC2 instance
 resource "aws_instance" "ubuntu_server" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-0914547665e6a707c"
   instance_type = "t2.micro"
 
   # Add a tag for easier identification
