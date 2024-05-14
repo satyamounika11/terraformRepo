83d64d6e3ac8970091e824f771a9530435df1f68
Sasi Datta <sasi.bitra@mitel.com>
2024-05-14 14:51:18 +0530

    format fix

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,16 @@
- # Create the EC2 instance
- resource "aws_instance" "ubuntu_server" {
-   ami           = "ami-06ef62a67be0325c9"
-   instance_type = "t2.micro"
+# Create the EC2 instance
+resource "aws_instance" "ubuntu_server" {
+  ami           = "ami-06ef62a67be0325c9"
+  instance_type = "t2.micro"
 
-   # Add a tag for easier identification
-   tags = {
-     Name = "MouniUbuntuVM"
-   }
+  # Add a tag for easier identification
+  tags = {
+    Name = "MouniUbuntuVM"
+  }
 
-#   # Attach a 30GB gp2 EBS volume
-   root_block_device {
-     volume_type = "gp2"
-     volume_size = 30
-   }
- }
+  #   # Attach a 30GB gp2 EBS volume
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 30
+  }
+}
